3b9c902f510d485d3160cafd2ae6f21a48c4e885
micen <duqujia@hotmail.com>
2025-02-27 17:32:57 +0800

    feat: Add Tag and PhotoTitle components

---
+++ components/PhotoTitle.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="absolute mb-2 top-0 left-0 pl-8 pr-12 pt-4 font-medium bg-gradient-to-r from-[#0081ff] to-[#1cbbb400] text-white">
+        <div class="text-shadow-md ">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/Tag.vue
@@ -0,0 +1,15 @@
+<template>
+    <span href="#"
+        class="hover:bg-zinc-400 text-sm w-fit p-4 py-2 font-bold mb-1 text-white" :class='value'>
+       {{item}}
+    </span>
+</template>
+
+<script setup>
+// const data = [["Ajax",'bg-violet-100'], ["SPA",'bg-blue-100'],["JavaScript",'bg-rose-100']]
+defineProps({
+    value: String,
+    item: String,
+    index: Number
+})
+</script>
