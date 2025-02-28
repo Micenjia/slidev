4161be3f411075f4fd4ab7ba00199b9a01ac6188
micen <duqujia@hotmail.com>
2025-02-28 10:27:33 +0800

    feat: Add BgGrid component

---
+++ components/BgGrid.vue
@@ -0,0 +1,8 @@
+<template>
+  <iframe
+    ref="framerIframe"
+    src="https://thistle-aims-758458.framer.app/grid"
+    frameborder="0"
+    class="absolute inset-0 h-120vh w-full z-[-1]"
+  ></iframe>
+</template>
